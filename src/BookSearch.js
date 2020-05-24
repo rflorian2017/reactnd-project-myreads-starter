27744a9b608c322290576386ca625917ea4db222
rflorian2017 <roby_florian@yahoo.com>
2020-05-24 18:50:44 +0300

    ready for review

---
+++ src/BookSearch.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI.js'
+
+class BookSearch extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        onChangeShelf: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: '',
+        searchedBooks: []
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+        if (query.length) {
+            BooksAPI.search(query).then((result) => {
+
+                const showingBooks = this.props.books
+                const currentlyReading = showingBooks.filter(book => book.shelf === 'currentlyReading')
+
+                const resultBooks = result.map(
+                    resultBook => {
+                        const book = showingBooks.find(showingBook => showingBook.id === resultBook.id) !== book
+                            ? currentlyReading
+                            : resultBook.shelf = 'none'
+
+                        return resultBook;
+                    })
+                this.setState(() =>
+                    (
+                        { searchedBooks: resultBooks }
+                    )
+                )
+            });
+        }
+    }
+
+    render() {
+        const { query, searchedBooks } = this.state
+        const { onChangeShelf } = this.props
+
+        const allBooks = query === ''
+            ? []
+            : searchedBooks;
+
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to='/'>
+                        <button className="close-search">Close</button>
+                    </Link>
+                    <div className="search-books-input-wrapper">
+                        {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                        <input type="text" placeholder="Search by title or author" value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)} />
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {allBooks.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select value={book.shelf} onChange={(event) => onChangeShelf(event, book)}>
+                                                <option value="move" disable="true">Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                            </li>
+
+                        ))
+                        }
+                    </ol>
+
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default BookSearch
---
+++ src/ListBooks.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+
+  render() {
+
+    const { books, onChangeShelf } = this.props
+
+    const currentlyReading = books.filter(book => book.shelf === 'currentlyReading')
+    const wantToRead = books.filter(book => book.shelf === 'wantToRead')
+    const read = books.filter(book => book.shelf === 'read')
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {currentlyReading.map((book) => (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={(event) => onChangeShelf(event, book)}>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {wantToRead.map((book) => (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={(event) => onChangeShelf(event, book)}>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {read.map((book) => (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select value={book.shelf} onChange={(event) => onChangeShelf(event, book)}>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to='/search'>
+            <button >Add a book</button>
+          </Link>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default ListBooks
